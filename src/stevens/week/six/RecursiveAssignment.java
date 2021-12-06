60580e05981f49f1efe003576a2898f9589af2e0
Kyle Stevens <kyle.stevens@perspecta.com>
2021-12-06 10:53:33 -0500

    add week six docs

--- src/stevens/week/six/RecursiveAssignment.java
+++ src/stevens/week/six/RecursiveAssignment.java
@@ -1,5 +1,9 @@
 package stevens.week.six;
 
+/**
+ * Program to create random array of 100 integers and recursively print them out
+ * @author Kyle Stevens
+ */
 import java.util.Random;
 
 public class RecursiveAssignment {
@@ -7,14 +11,27 @@ public class RecursiveAssignment {
 	private Integer[] intArray;
 	private Random r = new Random();
 	
+	/**
+	 * set the integer array
+	 * @param arr
+	 */
 	private void setIntArray(Integer[] arr) {
 		this.intArray = arr;
 	}
 	
+	/**
+	 * get the integer array
+	 * @return
+	 */
 	private Integer[] getIntArray() {
 		return intArray;
 	}
 	
+	/**
+	 * create an array, length n, of random integers
+	 * @param n
+	 * @return
+	 */
 	private Integer[] createRandomArray(int n) {
 		Integer[] intArray = new Integer[n];
 		for(int i = 0; i < n; i++) {
@@ -24,6 +41,10 @@ public class RecursiveAssignment {
 		return intArray;
 	}
 	
+	/**
+	 * print the array of length n out
+	 * @param n
+	 */
 	private void printArray(int n) {
 		if(n > 1) {
 			printArray(n - 1);
@@ -32,12 +53,15 @@ public class RecursiveAssignment {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("Kyle Stevens - Assignment 6: Program 1\n");
+
 		// call implicit constructor
 		RecursiveAssignment ra = new RecursiveAssignment();
 		
 		// set Integer[] equal to random array created by separate method of size 100
 		ra.setIntArray(ra.createRandomArray(100));
-		
+				
+		System.out.println("Random Array: ");
 		ra.printArray(ra.getIntArray().length);
 	}
 }
